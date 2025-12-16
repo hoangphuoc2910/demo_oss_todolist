cb19deedb8bca39265016e452558cf043c7fa339
hoangphuoc2910 <dohoangphuoc10@gmail.com>
2025-12-16 22:48:00 +0700

    Fix Program.cs syntax and sync .NET 8

--- Program.cs
+++ Program.cs
@@ -1,19 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Cấu hình CORS (Phải nằm TRƯỚC builder.Build)
-builder.Services.AddCors(options => {
-    options.AddPolicy("AllowInfinityFree",
-        policy => policy.WithOrigins("http://demogit.infinityfreeapp.com") // Domain InfinityFree của bạn
+// --- BẮT ĐẦU SỬA ---
+// 1. Đăng ký dịch vụ CORS (Phải bọc trong AddCors)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", // Đặt tên ngắn gọn là AllowAll
+        policy => policy.AllowAnyOrigin()  // Chấp nhận mọi nơi (InfinityFree, Localhost...)
                         .AllowAnyMethod()
                         .AllowAnyHeader());
 });
+// --- KẾT THÚC SỬA ---
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// 2. Kích hoạt CORS (Nằm trước MapControllers)
-app.UseCors("AllowInfinityFree");
+// 2. Kích hoạt CORS (Dùng đúng tên đã đặt ở trên)
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 app.MapControllers();
